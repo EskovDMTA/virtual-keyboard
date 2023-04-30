675c7b26d2eb4e45877f09774ca9b306cb489836
eskovdmt <eskovdmt@gmail.com>
2023-04-30 12:35:40 +0300

    feat: keyboard layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("From js")
+document.addEventListener("keydown", (event)=>{
+  console.log(event.code)
+  }
+)
+
+const buttons = document.querySelectorAll(".btn")
+const display = document.querySelector(".text-area")
+const deleteBtn = document.querySelector(".delete-btn")
+const backgroundColor = document.querySelector(".wrapper")
+const colorChanger = document.querySelector(".colors_input")
+let displayChars = []
+buttons.forEach(btn => {
+  btn.addEventListener("click", ()=>{
+    display.value += btn.innerText
+    displayChars = display.value.split("")
+    console.log(displayChars)
+  })
+})
+
+colorChanger.addEventListener("click", ()=>{
+console.log(colorChanger.value)
+  backgroundColor.style.backgroundColor = colorChanger.value
+})
